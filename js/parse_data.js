3610379ba22ebc4c0cb411ae09c0208fc372a4a3
Jesse Soldat <jsoldat@hotmail.com>
2015-11-04 16:02:09 -0500

    ajax and parse files

---
+++ js/parse_data.js
@@ -0,0 +1,9 @@
+const APP_ID = 'fRSjenryVsTgdJ8VwoUuSLlbE4tIFQ5xyuGFh8hO';
+const API_KEY = 'Zme0nXlBe6zFrdauItl9IOEmTXJon2lN2e99mht0';
+const APP_URL = 'https://api.parse.com/1/classes/user';
+
+export {
+  APP_ID,
+  API_KEY,
+  APP_URL
+};
