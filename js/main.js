eb362d81e2fbb460a9fbcb7a4b3ee616715fb6b5
Isaac Hardy <isaachardyDEV@gmail.com>
2015-11-04 16:46:23 -0500

    Added new router to start

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import moment from 'moment';
+import Router from './router';
 
+let element = document.querySelector('.app');
+
+new Router(element).start();
